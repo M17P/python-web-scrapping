ea55fdb2c02640619c271146c738396472e0907a
M17P <98769255+M17P@users.noreply.github.com>
2022-03-03 00:46:33 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 from bs4 import BeautifulSoup
-import requests
+from urllib.request import urlopen
 
 layout = [[sg.Text('Enter the url.')],
                  [sg.InputText()],
@@ -10,15 +10,33 @@ layout = [[sg.Text('Enter the url.')],
 window = sg.Window("Web Scrapping", layout)
 
 page_number = 1
-while True :
-    event,value = window.read()
-    print(event,value)
-    break
-window.read()
-window.read()
 
-event, values = window.read()
+while True :
+    event,values = window.read()
+
+    print(event)
+    print(values)
+    mydivs = str()
+    print(values)
+    model_to_brand_number = dict()
+
+    page_number = 1
+
+    url = "https://emalls.ir/%D9%85%D8%B4%D8%AE%D8%B5%D8%A7%D8%AA_Samsung-Galaxy-M30s-64GB~id~2805425"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    print(soup.get_text())
+
+    mydivs = soup.find_all("div", {"class": "c-price--value-wrapper"})
+    price_list = list
+    for span in mydivs:
+        try:
+            price_list.append(span.contents[0].split("\n")[1].strip())
+        except:
+            True
+        break
+
+window.read()
 window.close()
 
-text_input = values[0]
-sg.popup('You entered.if you enter Help i cannot help you :)', text_input)
